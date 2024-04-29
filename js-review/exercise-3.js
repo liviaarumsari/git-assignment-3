ec6eaddd111b1c682a55b92c9693c682d7d70ed1
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-04-29 08:12:28 +0700

    feat: add exercise 3 js review

---
+++ js-review/exercise-3.js
@@ -0,0 +1,11 @@
+// Buatlah function yang akan mencetak tanggal sekarang ketika dipanggil.
+function printCurrentDate() {
+    console.log(new Date());
+}
+printCurrentDate()
+
+// Buatlah function yang memberikan tanggal sekarang ketika dipanggil.
+function getCurrentDate() {
+    return new Date()
+}
+console.log(getCurrentDate());
